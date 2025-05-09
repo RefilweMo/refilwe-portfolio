6fc3f6b17d023ddefa7b398748b442a3965d7e07
Refilwe Molapisi <114024741+RefilweMo@users.noreply.github.com>
2025-05-09 19:55:48 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
-// Orange hover cursor
-const cursor = document.querySelector('.cursor-dot');
-
+// Cursor Effect
+const cursor = document.querySelector('.custom-cursor');
 document.addEventListener('mousemove', (e) => {
-  cursor.style.top = `${e.clientY}px`;
   cursor.style.left = `${e.clientX}px`;
+  cursor.style.top = `${e.clientY}px`;
+});
+
+// Tap animation for mobile
+document.querySelectorAll('.contact-buttons a').forEach(button => {
+  button.addEventListener('touchstart', () => {
+    button.classList.add('active');
+  });
+
+  button.addEventListener('touchend', () => {
+    button.classList.remove('active');
+  });
+
+  button.addEventListener('mousedown', () => {
+    button.classList.add('active');
+  });
+
+  button.addEventListener('mouseup', () => {
+    button.classList.remove('active');
+  });
+
+  button.addEventListener('mouseleave', () => {
+    button.classList.remove('active');
+  });
 });
