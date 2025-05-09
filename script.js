912bda869e0e7b8b76a75b3fc1a5e93805fa599e
Refilwe Molapisi <114024741+RefilweMo@users.noreply.github.com>
2025-05-09 19:07:35 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Orange hover cursor
+const cursor = document.querySelector('.cursor-dot');
+
+document.addEventListener('mousemove', (e) => {
+  cursor.style.top = `${e.clientY}px`;
+  cursor.style.left = `${e.clientX}px`;
+});
